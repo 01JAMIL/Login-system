43f92e11c0cd0a648cf84ecdb5f0b0b9f1f071d6
01JAMIL <jamilbenbrahim1@gmail.com>
2023-01-01 19:38:42 +0100

    Fix a problem

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -7,67 +7,50 @@ const { validateRegisterForm, validateLoginForm } = require('../validation/user.
 
 const register = asyncHandler(async (req, res) => {
     const { errors, valid } = validateRegisterForm(req.body)
+    if (!valid) {
+        return res.status(400).json(errors)
+    }
 
-    try {
-        if (!valid) {
-            return res.status(400).json(errors)
+    await User.findOne({ email: req.body.email }).then(async (user) => {
+        if (user) {
+            return res.status(400).json({ existEmailError: 'This email already exists' })
         }
 
-        await User.findOne({ email: req.body.email }).then(async (user) => {
-            if (user) {
-                return res.status(400).json({ existEmailError: 'This email already exists' })
-            }
-
-            const newUser = new User(req.body)
-            const salt = await bcrypt.genSalt(10)
-            const cryptedPassword = await bcrypt.hash(newUser.password, salt)
-            newUser.password = cryptedPassword
-            await User.create(newUser).then(() => {
-                res.status(200).json({ token: generateToken(newUser._id) })
-            }).catch(err => {
-                res.status(400).json({ error: 'Something went wrong when creating your account.' })
-            })
+        const newUser = new User(req.body)
+        const salt = await bcrypt.genSalt(10)
+        const cryptedPassword = await bcrypt.hash(newUser.password, salt)
+        newUser.password = cryptedPassword
+        await User.create(newUser).then(() => {
+            res.status(200).json({ token: generateToken(newUser._id) })
+        }).catch(err => {
+            res.status(400).json({ error: 'Something went wrong when creating your account.' })
         })
-
-
-    } catch (error) {
-        res.status(400).json({ error: error })
-    }
+    })
 })
 
 const login = asyncHandler(async (req, res) => {
     const { errors, valid } = validateLoginForm(req.body)
-    try {
-
-        if (!valid) {
-            return res.status(400).json(errors)
-        }
-
-        await User.findOne({ email: req.body.email }).then(async (user) => {
-            if (user && (await bcrypt.compare(req.body.password, user.password))) {
-                res.status(200).json({ token: generateToken(user._id) })
-            } else {
-                res.status(400).json({ badCredentials: 'Email or password incorrect' })
-            }
-        })
-
-    } catch (error) {
-        res.status(400).json({ error: error })
+    if (!valid) {
+        return res.status(400).json(errors)
     }
+
+    await User.findOne({ email: req.body.email }).then(async (user) => {
+        if (user && (await bcrypt.compare(req.body.password, user.password))) {
+            res.status(200).json({ token: generateToken(user._id) })
+        } else {
+            res.status(400).json({ badCredentials: 'Email or password incorrect' })
+        }
+    })
 })
 
 const getMe = asyncHandler(async (req, res) => {
-    try {
-        await User.findOne({ _id: req.user._id }).then(user => {
-            if (user) {
-                return res.status(200).json({ user: user })
-            }
+    await User.findOne({ _id: req.user._id }).then(user => {
+        if (user) {
+            return res.status(200).json({ user: user })
+        }
 
-            res.status(400).json({ error: 'User not exist' })
-        })
-    } catch (error) {
-
-    }
+        res.status(400).json({ error: 'User not exist' })
+    })
 })
 
 const generateToken = (id) => {
